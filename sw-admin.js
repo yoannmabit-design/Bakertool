1a654dcfc93cfa645853ed829f8fcb9c7240c173
yoannmabit-design <yoannmabit@gmail.com>
2026-09-14 21:44:45 +0800

    Update sw-admin.js

--- sw-admin.js
+++ sw-admin.js
@@ -16,7 +16,7 @@
 
    Attention : ne jamais faire passer les pages HTML en cache d'abord.
    Le régime 1 est un choix délibéré. */
-const VERSION = 'yfb-admin?v=2';
+const VERSION = 'yfb-admin?v=3';
 
 const ESSENTIELS = [
   './', './index.html',
